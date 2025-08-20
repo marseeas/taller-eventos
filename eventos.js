5ed59b16e70b5b75928ece3bf4e8220cd517fa3c
marseeas <g06mar@gmail.com>
2025-08-19 23:56:18 -0300

    click solucion

--- eventos.js
+++ eventos.js
@@ -3,3 +3,10 @@ let divs = document.getElementsByClassName("midiv");
 divs[0].addEventListener("click", function () {
   alert("Hola! Soy el div");
 });
+
+let boton = divs[0].querySelector("button");
+
+boton.addEventListener("click", function (event) {
+  alert("Hola!");
+  event.stopPropagation();
+});
